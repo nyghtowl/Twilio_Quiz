508cb3970fdd391ebbfb0536c957876b45639a49
nyghtowl <warrick.melanie@gmail.com>
2013-10-08 20:09:40 -0700

    Adding print to debug and pulled encode.

--- app/views.py
+++ app/views.py
@@ -82,9 +82,13 @@ def quiz_game():
 
             message = "Correct Answer. You have %d points out of 30. %s" % (score, questions[game_round])
             # if won give trophy:
+            print 100, message
             chosen_index = random.randint(0, num_trophies-1)
-            chosen_trophy = trophy_chars[chosen_index*2:(chosen_index+1)*2].encode('utf-8')
+            print 101, chosen_index
+            chosen_trophy = trophy_chars[chosen_index*2:(chosen_index+1)*2]
+            print 102, chosen_trophy
             message += chosen_trophy
+            print 103, message
         else:
             score = session[from_number]
             message = "Wrong answer. We were looking for %s. Your score is %d out of 30. %s" % (print_answers[game_round], score, questions[game_round])
